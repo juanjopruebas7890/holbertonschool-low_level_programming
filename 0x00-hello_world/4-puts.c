6afaf95550acf55c3144db6904daa335c6b9dc9c
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-05 20:44:09 +0000

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stidio.h>
 
-int main(void)
+int main (void)
 {
 
-	puts("Programming is like building a multilingual puzzle\n);
-        return(0);
+	puts ("/"Programming is like building a multilingual puzzle\n");
+        return 0;
 
 }
