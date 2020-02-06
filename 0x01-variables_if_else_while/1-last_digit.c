9bc3e0dda0977355c3c2da28d3a3d08fc038b6a0
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-06 19:38:12 +0000

    else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ else if (l == 0)
 {
 	printf("Last digit of %i is %i and is zero\n", n, z);
 }
-else
+else if (l < 6 || l != 0)
 {
 	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, z);
 }
