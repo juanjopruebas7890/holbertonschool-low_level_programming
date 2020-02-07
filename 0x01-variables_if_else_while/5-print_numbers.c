8618465b53a42f8891426527300bc40bc5a99840
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-07 13:54:01 +0000

    else

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	int a;
+        int a;
 
-	for (a = 0; a < 10; a++)
-	printf("%i", a);
-	printf("\n");
-	return (0);
+        for (a = 0; a < 10; a++)
+		printf("%i", a);
+        printf("\n");
+        return (0);
 }
