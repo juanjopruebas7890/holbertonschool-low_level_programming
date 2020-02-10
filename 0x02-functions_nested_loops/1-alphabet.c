af17d912b4efb02f93fbe64c188e2ead1f5ac738
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-10 22:24:33 +0000

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+		_putchar(a++);
+	_putchar('\n');
+
+}
