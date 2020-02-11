d584236f2d16e1c10ca71394dfbac765b1e0b161
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-11 14:56:45 +0000

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - is a function that will print the alphabet 10 times
+ * @void: null
+ */
+void print_alphabet_x10(void)
+{
+
+	char a;
+		int f = 0;
+
+		while (f < 9)
+		{
+			for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+
+		_putchar('\n');
+		f++;
+		}
+}
