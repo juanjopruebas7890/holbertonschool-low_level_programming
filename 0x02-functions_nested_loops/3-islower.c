623922c066b500698ea5850efbb5a72ba64c6819
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-11 15:32:59 +0000

    loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _islower - this function sees if c meets certain conditions
+ * @c: null
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
