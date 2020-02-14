0825c8ae2a61f43b52d15d77e96257968d7369be
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-14 14:07:15 +0000

    loops2

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int p;
+
+	for (p = 1; p <= 100; p++)
+	{
+		if (p % 3 == 0 && p % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (p % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (p % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (p != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
