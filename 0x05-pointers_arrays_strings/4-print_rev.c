c39f75c7e163cd03b447d2dd3b8365d7c730317b
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-19 20:21:57 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - is the prototype
+ *@s: is the variable we are working with
+ *
+ */
+void print_rev(char *s)
+{
+	int r;
+
+
+	for (r = 0; s[x] != '\0' ; r++)
+	{
+	}
+
+	for (r = r - 1; r >= 0; r--)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
+}
