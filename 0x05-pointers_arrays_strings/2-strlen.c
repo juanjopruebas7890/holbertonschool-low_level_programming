b1cdf0c72b0bddc616d5ded265a67e19ae75800d
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-19 20:18:31 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_strlen - It will tell me how long is a string of characters
+ *@s: It will call the word that is on the main
+ *Return: n
+ */
+int _strlen(char *s)
+{
+	int n;
+
+
+	for (n = 0; *(s + n) != '\0'; n++)
+	{
+	}
+	return (n);
+}
