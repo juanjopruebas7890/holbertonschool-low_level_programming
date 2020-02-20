cb90e87e931c9cdc53f7bc857fa060a23d65eac3
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-20 19:37:54 +0000

    arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,10 @@
 #include <string.h>
 /**
  * _strcat - Is the prototype, concatenates two strings.
- * @_strlen - Is another protptype we are using.
- * strlen: Is refering to a variable
  * @src: Is the variable we are working with
- * dest: Is the second variable we are working with
+ * @dest: Is the second variable we are working with
  * Return: 0
  */
-int _strlen(char *src);
 char *_strcat(char *dest, char *src)
 {
 	int j;
@@ -22,10 +19,12 @@ char *_strcat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
-int _strlen(char *src)
 /**
- *strlen - Is a doc line
+ *_strlen - It will tell me how long is a string of characters
+ *@src: It will call the word that is on the main
+ *Return: n
  */
+int _strlen(char *src)
 {
 	int n;
 
