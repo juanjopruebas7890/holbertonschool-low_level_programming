38155632613370fc054036ca2f398952605d1976
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-24 16:07:43 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * _strchr - Is the prototype, will return to a pointer form c to s.
+ * @s: Is the variable we are working with
+ * @c: Is the second variable we are working with
+ * p - The variable that I declared..
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int p;
+
+	for (p = 0; *(s + p) != '\0'; p++)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
+}
