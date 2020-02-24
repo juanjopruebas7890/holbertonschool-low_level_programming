5f4e12dce401d7334ff3b99825d4e8e3b2579523
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-24 22:33:37 +0000

    9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * print_chessboard - Is the prototype.
+ * @a: Is the variable we are working with
+ * @: Is the second variable we are working with
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
