3eb23b7d628f26880b89598144d455ed04fce504
Juan Jose Villegas <1561@holbertonschool.com>
2020-02-24 19:21:45 +0000

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * _strspn - Is the prototype.
+ * @s: Is the variable we are working with
+ * @accept: Is the second variable we are working with
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int k = 0;
+	int v;
+	int p;
+
+	for (v = 0; s[v]; v++)
+	{
+		for (p = 0; p < 5; p++)
+		{
+			if (accept[p] == s[v])
+				break;
+		}
+		k = k + 1;
+	}
+	return (p);
+}
