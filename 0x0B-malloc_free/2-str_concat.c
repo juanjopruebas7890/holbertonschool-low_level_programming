8c7dccda1eefe377391c7da99dc8393ca621b2d2
Juan Jose Villegas <1561@holbertonschool.com>
2020-03-04 00:21:23 +0000

    fix5

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,8 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (c = 0; s1[c]; c++)
 		;
-       	for (w = 0; s2[w]; w++)
+	for (w = 0; s2[w]; w++)
+		;
 	c++;
 	f = malloc(sizeof(char) * (c + w));
 	if (f == NULL)
