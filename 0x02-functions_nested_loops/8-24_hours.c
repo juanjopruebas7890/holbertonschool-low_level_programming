a260efecf64b7a672db50bc564264080f4f5ec9e
Juan Jose Villegas <1561@holbertonschool.com>
2020-03-08 18:44:33 +0000

    fix all code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,31 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int t1;
-	int t2;
-	int t3;
-	int t4;
+	int h, m;
 
-	for (t1 = '0'; t1 <= '9'; t1++)
+	for (h = 0; h < 24; h++)
 	{
-		for (t2 = '0'; t2 <= '9'; t2++)
+		for (m = 0; m <= 59; m++)
 		{
-			for (t3 = '0'; t3 <= '5'; t3++)
-			{
-				for (t4 = '0'; t4 <= '9'; t4++)
-				{
-					if ((t1 != '2' && t2 <= '9') || (t1 == '2' && t2 <= '3'))
-					{
-						_putchar(t1);
-						_putchar(t2);
-						_putchar(':');
-						_putchar(t3);
-						_putchar(t4);
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
-
 }
