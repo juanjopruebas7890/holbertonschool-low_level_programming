1480f044dab70f39eb619d61539c0647e2b0def0
Juan Jose Villegas <1561@holbertonschool.com>
2020-03-11 17:57:51 +0000

    name change

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
