6841d4d91577072be134f800fd51f03dda71cda4
Juan Jose Villegas <1561@holbertonschool.com>
2020-03-13 21:34:00 +0000

    Write a function that prints strings, followed by a new line.

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - Sum numbers.
+ * @separator: variable.
+ * @n: Varaible
+ * Return: 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list lista;
+	unsigned int i;
+	char *p;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	va_start(lista, n);
+
+	for (i = 0; i < n; i++)
+	{
+		p = va_arg(lista, char *);
+		printf("%s", p);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(lista);
+}
