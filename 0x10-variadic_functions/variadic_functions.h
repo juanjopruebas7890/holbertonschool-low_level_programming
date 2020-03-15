03aeb458e3fb872dd2651dcb28c1bc4d289b1123
Juan Jose Villegas <1561@holbertonschool.com>
2020-03-15 13:30:07 +0000

    estruct add

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H_FILE
 #define VARIADIC_FUNCTIONS_H_FILE
 
+/**
+ * struct est- Numbers structure
+ * @c: It's a variable char
+ * @p: It's a function void
+ */
+typedef struct est
+{
+	char c;
+	void (*p)(va_list list);
+} st_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
