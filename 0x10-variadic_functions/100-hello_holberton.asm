963ed980e720cfd4bb026113669822d8e1f84347
Juan Jose Villegas <1561@holbertonschool.com>
2020-03-15 14:40:28 +0000

    code fix

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,26 @@
+%define sys_write 1
+	%define stdout 1
 
-	          global    main
-	          extern    puts
+	%define sys_exit 60
+	%define success 0
 
-	          section   .text
-main:	
-	          mov       rdi, message
-	          call      puts	 
-	          ret			 
-message:
-	          db        "Hello, Holberton", 0
+	%define nl 10
+
+	section .data
+
+	    message db "Hello, Holberton", nl
+
+
+	section .text
+
+	GLOBAL main
+main:
+	    mov rax, sys_write
+	    mov rdi, stdout
+	    mov rsi, message
+	    mov rdx, 17
+	    syscall
+
+	    mov rax, sys_exit
+	    mov rdi, success
+	    syscall
