de411b3230460ff111428197ddc8e8db2a0011b8
Juan Jose <1561@holbertonschool.com>
2020-03-25 00:27:54 +0000

    first add

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - Will delete a node at index.
+ * @head: Variable.
+ * @index: Variable.
+ * Return: current.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *c, *temporal;
+	unsigned int p = 0;
+
+	c = *head;
+
+	if (c == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		c = c->next;
+		free(c);
+		return (1);
+	}
+	while (c)
+	{
+		for (p = 0; c != NULL; p++)
+		{
+			if (p == index -1)
+			{
+				temporal = c->next;
+				c->next = temporal->next;
+				free(temporal);
+				return (1);
+			}
+			c = c->next;
+		}
+	}
+	return (-1);
+}
