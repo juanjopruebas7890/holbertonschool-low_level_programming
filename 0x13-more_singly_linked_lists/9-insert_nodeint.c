ce46880b99ae0403ed80e8c78dac4352c1588103
Juan Jose <1561@holbertonschool.com>
2020-03-25 00:00:37 +0000

    re-do last change

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		current = node->next;
-		*head = node;
+		current = node;
 		return (node);
 	}
 
@@ -38,5 +38,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		p++;
 	}
-	return (node);
+	return (NULL);
 }
