66b24cb55625b7f9f5f0c5c21ac22bbf2a92a008
Juan Jose <1561@holbertonschool.com>
2020-03-25 00:29:23 +0000

    betty fixed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		for (p = 0; c != NULL; p++)
 		{
-			if (p == index -1)
+			if (p == index - 1)
 			{
 				temporal = c->next;
 				c->next = temporal->next;
