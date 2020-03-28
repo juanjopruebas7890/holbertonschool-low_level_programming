a93c747378eaeef915f75a347f3e7e7332a27643
Juan Jose <1561@holbertonschool.com>
2020-03-28 23:50:10 +0000

    advanced3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, r, w;
+
+	for (i = '0'; i <= '9'; i++)
+		for (j = '0'; j <= '9'; j++)
+			for (r = i; r <= '9'; r++)
+				for (w = j; w <= '9'; w++)
+				{
+					if (!((j == d) && (i == c)))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(r);
+						putchar(w);
+
+						if (!(i == '9' && j == '8'))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+	putchar('\n');
+	return (0);
+}
