a38ddc7c9da1e3108ee6512c96bd7022c3d81534
Juan Jose <1561@holbertonschool.com>
2020-03-29 22:23:06 +0000

    add prototypes

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -14,4 +14,8 @@ char *leet(char *);
 char *rot13(char *);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *src);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif
