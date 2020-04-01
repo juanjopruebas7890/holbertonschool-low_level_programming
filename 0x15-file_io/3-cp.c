00c7f6318e727706fc997a9bb934b75fd1b53af1
Juan Jose <1561@holbertonschool.com>
2020-04-01 23:00:03 +0000

    fix: exits

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	i = open(argv[1], O_RDONLY);
 	if (i == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[2]), exit(99);
+			argv[2]), exit(98);
 	j = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (j == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		rd = read(i, buf, 1024);
 		if (rd == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]), exit(99);
+				argv[1]), exit(98);
 		wr = write(j, buf, rd);
 		if (wr == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
