8413c30baa55446dc4711e43a6c1560625ddfe6a
Juan Jose <1561@holbertonschool.com>
2020-04-01 02:27:06 +0000

    fix code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,20 +15,22 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	i = open(filename, O_WRONLY | O_APPEND);
-
 	if (i == -1)
 	{
 		return (-1);
 	}
 
-	for (s = 0; *(text_content + s); s++)
+	if (text_content != NULL)
 	{
-	}
+		for (s = 0; *(text_content + s); s++)
+		{
+		}
 
-	wr = write(i, text_content, s);
-	if (wr == -1)
-	{
-		return (-1);
+		wr = write(i, text_content, s);
+		if (wr == -1)
+		{
+			return (-1);
+		}
 	}
 	close(i);
 	return (1);
