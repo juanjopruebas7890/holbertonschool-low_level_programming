0774cc84fb09dc790cd5b3e82be3f277a45a686a
Juan Jose <1561@holbertonschool.com>
2020-04-10 23:58:30 +0000

    betty fixed

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,40 +1,30 @@
 #include "holberton.h"
 /**
- * infinite_add - adds two strings
- * @n1: string 1
- * @n2: string 2
- * @r: added string
- * @size_r: buffer of string
- * Return: r
+ * infinite_add - Will add two numbers
+ * @n1: variable
+ * @n2: variable
+ * @r: variable
+ * @size_r: variable
+ * Return: 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a, i = 0, j = 0, k = 0, l, m, s, c = 0, d1 = 0, d2 = 0;
 
-        while (n1[i] != '\0')
-	{
+	while (n1[i] != '\0')
 		i++;
-	}
 	while (n2[j] != '\0')
-	{
 		j++;
-	}
 	if (i + 2 > size_r || j + 2 > size_r)
-	{
 		return (0);
-	}
 	i = i - 1;
 	j = j - 1;
 	while (i >= 0 || j >= 0)
 	{
 		if (i >= 0)
-		{
 			d1 = n1[i--] - '0';
-		}
 		if (j >= 0)
-		{
 			d2 = n2[j--] - '0';
-		}
 		s = d1 + d2 + c;
 		if (s > 9)
 		{
@@ -42,12 +32,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			s = s - 10;
 		}
 		else
-		{
 			c = 0;
-		}
 		r[k++] = (a + '0');
 		m = k;
-		k = k -1;
+		k = k - 1;
 		for (l = 0; l < k; l++, k--)
 		{
 			a = r[k];
