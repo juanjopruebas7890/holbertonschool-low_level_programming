4d3ab9552fc7a077a42f1deda3c14fe2ae89b63e
Juan Jose <1561@holbertonschool.com>
2020-04-13 03:38:09 +0000

    fix code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,31 +6,22 @@
  */
 int main(void)
 {
-	int i, j, r, w;
+	int i;
+	int j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			for (r = '0'; r <= '9'; r++)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
 			{
-				for (w = '0'; w <= '9'; w++)
-				{
-					if (i < j || (j == r && j < w))
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(r);
-						putchar(w);
-
-						if (!(i == '9' && j == '8' && r == '9' && w == '9'))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
