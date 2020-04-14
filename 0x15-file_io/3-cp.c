119905ae2d1bc67cbd6921b0d61745aa1ca58e7e
Juan Jose <1561@holbertonschool.com>
2020-04-14 18:58:57 +0000

    fix code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,31 +7,43 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, wr, rd = 1024;
+	int i, j, c1, c2;
+	ssize_t rd, wr;
 	char buf[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	i = open(argv[1], O_RDONLY);
-	if (i == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[2]), exit(98);
-	j = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (j == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while (rd == 1024)
+	i = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	j = open(argv[1], O_RDONLY);
+	if (i == -1 || j == -1)
 	{
-		rd = read(i, buf, 1024);
+		if (i == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[2]), exit(99);
+		if (j == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[1]), exit(98);
+	}
+	while ((rd = read(j, buf, 1024)) != 0)
+	{
 		if (rd == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 				argv[1]), exit(98);
-		wr = write(j, buf, rd);
+		wr = write(i, buf, rd);
 		if (wr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[2]), exit(99);
 	}
-	if (close(i) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i), exit(100);
-	if (close(j) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j), exit(100);
+	c1 = close(i);
+	c2 = close(j);
+	if (c1 == -1 || c2 == -1)
+	{
+		if (c1 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+		if (c2 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j);
+		exit(100);
+	}
 	return (0);
 }
+
