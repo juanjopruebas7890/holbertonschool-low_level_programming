d686fa493c205933be8d6f2f0fa0e26008ed4a00
Juan Jose <1561@holbertonschool.com>
2020-04-14 18:07:11 +0000

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * words -  will print the string
+ */
+void words(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
