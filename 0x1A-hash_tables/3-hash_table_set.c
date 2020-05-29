393a563ad99120919fa717dcf1266cc0cba8c373
Juan Jose <1561@holbertonschool.com>
2020-05-29 18:22:36 +0000

    [add] new code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,29 +9,51 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
+	hash_node_t *n;
 	hash_node_t *tmp;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL)
 	{
 		return (0);
 	}
-	
-	i = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[i] != NULL && strcmp(ht->array[i]->key, key) == 0)
-	{
-		free(ht->array[i]->value);
-		ht->array[i]->value = strdup(value);
-		return (1);
-	}
-	tmp = malloc(sizeof(hash_node_t));
-	if (tmp == NULL)
+	if (key == NULL || key[0] == '\0')
 	{
 		return (0);
 	}
-	tmp->key = strdup(key);
-	tmp->value = strdup(value);
-	tmp->next = ht->array[i];
-	ht->array[i] = tmp;
+	if (ht->size == 0 || ht->array == NULL)
+	{
+		return (0);
+	}
+
+	i = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+	}
+
+	n = malloc(sizeof(hash_node_t));
+	if (n == NULL)
+	{
+		return (0);
+	}
+	n->key = strdup(key);
+	n->value = strdup(value);
+
+	if (ht->array[i] != NULL)
+	{
+		n->next = ht->array[i];
+	}
+	else
+	{
+		n->next = NULL;
+	}
+	ht->array[i] = n;
 	return (1);
 }
