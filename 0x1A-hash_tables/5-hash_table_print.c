ee2238867ac5ab1741ab942a54690599aee91a84
Juan Jose <1561@holbertonschool.com>
2020-05-29 17:49:01 +0000

    [add] first version

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - will retrieve an value associated with a key
+ * @ht: variable
+ * Return: no return
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *tmp;
+	char *c = "";
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			printf("%s'%s': '%s'", c, tmp->key, tmp->value);
+			c = ", ";
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
